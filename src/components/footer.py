a1eb4d996a3d77c4e1022c0e724cb0ddeaa7a0b0
Harshit Pundir <harshitpundir36@gmail.com>
2026-05-06 05:03:28 +0530

    Initial commit

---
+++ src/components/footer.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+def footer_home():
+
+    st.markdown("""
+        <div style="
+            margin-top:2rem;
+            display:flex;
+            gap:6px;
+            justify-content:center;
+            align-items:center;
+        ">
+            <p style="
+                font-weight:bold;
+                color:white;
+                margin:0;
+            ">
+                Created with ❤️ by Harshit Pundir
+            </p>
+        </div>
+    """, unsafe_allow_html=True)
